434faf19a0152ffec2f0d13cdde8f1fa6676a61f
Kefirphy <vid08msk@gmail.com>
2026-09-04 21:36:18 +0300

    Добавлено 1.2.2 и 1.2.3

---
+++ 123-led-button-debounce/main.c
@@ -0,0 +1,44 @@
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
+const uint LED_PIN = 25;
+const uint BUTTON_PIN = 15;
+
+const uint DEBOUNCE_MS = 20;
+
+bool get_button_debounce(uint pin)
+{
+    bool state = gpio_get(pin);
+    sleep_ms(DEBOUNCE_MS);
+    return state && gpio_get(pin);
+}
+
+int main()
+{
+    // Инициализация светодиода на плате
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    // Инициализация кнопки
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN); // Теперь пин работает на прием сигнала
+    gpio_pull_up(BUTTON_PIN);          // Включаем подтягивающий резистор
+
+    bool led = false;
+    bool previous = false;
+
+    while (1)
+    {
+        // Читаем текущее напряжение на пине кнопки
+         bool current = get_button_debounce(BUTTON_PIN);
+
+        // Если раньше было напряжение (кнопка отпущена), а теперь нет (кнопка нажата)
+        if (previous == true && current == false)
+        {
+            led = !led;             // Меняем флаг светодиода на противоположный
+            gpio_put(LED_PIN, led); // Применяем флаг к реальному светодиоду
+        }
+
+        previous = current; // Запоминаем состояние для следующего витка цикла
+    }
+}
